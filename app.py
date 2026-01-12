74c88c46a81c3f82c8fcdc7a30b1392cde99d0d1
sando5 <daniel.sandoval.sd@gmail.com>
2026-01-12 11:52:00 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -117,4 +117,5 @@ def analyze_url():
     return render_template("index.html", articles=articles)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
